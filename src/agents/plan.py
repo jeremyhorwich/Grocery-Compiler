8205b4ffdc89af14cd5856c0526019905caea4b5
jeremyhorwich <jeremyahorwich@gmail.com>
2026-01-11 12:31:07 -0600

    Break out full recipes

--- src/agents/plan.py
+++ src/agents/plan.py
@@ -1,9 +1,9 @@
 from .agent import Agent
 
-def create_recipes_agent() -> Agent:
-    """Create a recipes agent."""
+def create_meal_plan_agent() -> Agent:
+    """Create a meal plan agent."""
 
-    RECIPES_AGENT_TEMPERATURE = 0.5
+    RECIPES_AGENT_TEMPERATURE = 0.6
 
     return Agent(
         __name__, 
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from agents.ingredients import create_ingredients_agent
+from agents.plan import create_meal_plan_agent
 from agents.recipes import create_recipes_agent
 from emails import send_email
 
 def main():
-    ingredients_agent = create_ingredients_agent()
+    print("Invoking ingredients agent...")
 
+    ingredients_agent = create_ingredients_agent()
     ingredients_result = ingredients_agent.invoke({
         "messages": [
             {"role": "user", "content": "Construct the list, please."}
@@ -15,18 +17,37 @@ def main():
 
     ingredients_result_content = ingredients_result["messages"][-1].content
 
-    recipes_agent = create_recipes_agent()
+    print("Invoking meal plan agent...")
 
-    recipes_result = recipes_agent.invoke({
+    meal_plan_agent = create_meal_plan_agent()
+    meal_plan_result = meal_plan_agent.invoke({
         "messages": [
             {"role": "user", "content": ingredients_result_content}
         ]
     })
 
+    print("Meal plan agent finished...")
+
+    meal_plan_content = meal_plan_result["messages"][-1].content
+   
+    meal_plan_text_split = meal_plan_content.split("++++++", 1)
+    if len(meal_plan_text_split) != 2:
+        print("Error: Unexpected recipes agent output format.")
+        print(meal_plan_content)
+        return
+    send_email(meal_plan_text_split[0])
+
+    print("Invoking recipes agent...")
+
+    recipes_agent = create_recipes_agent()
+    recipes_result = recipes_agent.invoke({
+        "messages": [
+            {"role": "user", "content": meal_plan_text_split[1]}
+        ]
+    })
+
     print("Recipes agent finished...")
-
-    recipes_content = recipes_result["messages"][-1].content
-
-    send_email(recipes_content)
+    
+    print(recipes_result["messages"][-1].content)
 
 main()
